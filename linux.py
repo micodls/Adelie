c06eea6b24bc171649b763130a5e87ca4ab04ae1
Mico de los Santos <mico.dlsantos@gmail.com>
2015-08-05 17:51:48 +0800

    Added linux module

--- linux.py
+++ linux.py
@@ -1,16 +1,10 @@
 #!/usr/bin/env python
 
-import time
-import os
-import sys
-import subprocess
-import shlex
 import helpers
-import linux
 
 class Installer:
     def __init__(self):
-        print ''
+        print 'HERE'
         # self.__update()
         # self.__upgrade()
         # self.__clean()
@@ -90,61 +84,3 @@ class Installer:
             print '[WARNING]: {} not [yet] supported'.format(alias)
 
         return command_name
-
-class BitBucket:
-    def __init__(self):
-        try:
-            helpers.execute('pip show bitbucket-api')
-        except ImportError as e:
-            print e
-
-# Fix this one
-class Git:
-    def __init__(self):
-        print ''
-        # self.__config()
-        # self.__generate_SSH()
-
-    def __config(self):
-        helpers.execute('git config --global user.name "Mico de los Santos"')
-        helpers.execute('git config --global user.email mico.dlsantos@gmail.com')
-
-    def __generate_SSH(self):
-        helpers.execute('ssh-keygen')
-
-    def clone(self, source, dest, SSH=True):
-        helpers.execute('git clone {} {}'.format(source, dest if dest else ''))
-
-class Timer:
-    def __enter__(self):
-        self.start_time = time.time()
-
-    def __exit__(self, type, value, traceback):
-        elapsed_time = time.time() - self.start_time
-        minutes, seconds = divmod(elapsed_time, 60)
-        print "Elapsed Time: %02dm %02ds" % (minutes, seconds)
-
-def main():
-    with Timer():
-        linux.Installer()
-        # git = Git()
-        # git.clone()
-
-if __name__ == '__main__':
-    main()
-
-# skype
-# viber
-# spotfy
-
-# http://tecadmin.net/install-laravel-framework-on-ubuntu/
-# https://bitbucket-api.readthedocs.org/en/latest/usage.html
-# https://pypi.python.org/pypi/bitbucket-api/0.5.0
-# https://getcomposer.org/doc/03-cli.md#install
-# https://confluence.atlassian.com/display/BITBUCKET/Set+up+SSH+for+Git
-# https://help.github.com/articles/generating-ssh-keys/
-# http://askubuntu.com/questions/527551/how-to-access-a-git-repository-using-ssh
-# http://tipsonubuntu.com/2015/03/24/install-skype-4-3-in-ubuntu-15-04/
-# http://askubuntu.com/questions/293693/how-to-install-skype-with-ubuntu-13-04
-# http://askubuntu.com/questions/91543/apt-get-update-fails-to-fetch-files-temporary-failure-resolving-error
-
