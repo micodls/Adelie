59bdb8b53d591f14ec367491856e85ac97bf1fef
Mico de los Santos <mico.dlsantos@gmail.com>
2016-01-12 14:12:07 +0800

    Populated command and installer names according to initial list

--- linux.py
+++ linux.py
@@ -12,110 +12,137 @@ class Installer:
         # self.__clean()
 
         # generic - works
-        # self.__install('vim')
-        # self.__install('curl')
-        # self.__install('git')
+        # self.__install("vim")
+        # self.__install("curl")
+        # self.__install("git")
 
-        # self.__install('python-pip')
-        # self.__install('python-setuptools')
+        # self.__install("python-pip")
+        # self.__install("python-setuptools")
 
         # google chrome
-        # helpers.execute('wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -')
-        # helpers.execute('sudo sh -c \'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google-chrome.list\'')
+        # helpers.execute("wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -")
+        # helpers.execute("sudo sh -c \"echo "deb http://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google-chrome.list\"")
         # self.__update()
-        # self.__install('google chrome')
+        # self.__install("google chrome")
 
         # sublime
-        # helpers.execute('sudo add-apt-repository ppa:webupd8team/sublime-text-3')
+        # helpers.execute("sudo add-apt-repository ppa:webupd8team/sublime-text-3")
         # self.__update()
-        # self.__install('sublime')
+        # self.__install("sublime")
 
         # vlc
-        # self.__install('vlc')
+        # self.__install("vlc")
 
         # skype - not working
-        # helpers.execute('sudo add-apt-repository "deb http://archive.canonical.com/ $(lsb_release -sc) partner"')
+        # helpers.execute("sudo add-apt-repository "deb http://archive.canonical.com/ $(lsb_release -sc) partner"")
         # self.__update()
-        # self.__install('skype')
+        # self.__install("skype")
 
         # spotify - not working (needs to be updated for 15.04)
-        # helpers.execute('sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys D2C19886')
-        # helpers.execute('sudo sh -c \'echo "deb http://repository.spotify.com stable non-free" > /etc/apt/sources.list.d/spotify.list\'')
+        # helpers.execute("sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys D2C19886")
+        # helpers.execute("sudo sh -c \"echo "deb http://repository.spotify.com stable non-free" > /etc/apt/sources.list.d/spotify.list\"")
         # self.__update()
-        # self.__install('spotify')
+        # self.__install("spotify")
 
     def __install(self, alias):
+        print alias
         command = self.__get_command_name(alias)
         installer = self.__get_installer_name(alias)
 
-    print command, installer
+        print command, installer
         #if command is None or installer is None:
-        #    helpers.log('{} not [yet] supported'.format(alias), 'warning')
+        #    helpers.log("{} not [yet] supported".format(alias), "warning")
         #else:
         #    try:
-        #        helpers.log('Checking if {} is not yet installed.'.format(alias), 'debug')
-        #        helpers.execute('{} --help'.format(command), 'quiet')
+        #        helpers.log("Checking if {} is not yet installed.".format(alias), "debug")
+        #        helpers.execute("{} --help".format(command), "quiet")
         #    except OSError as e:
         #        if e.errno == os.errno.ENOENT:
-        #            helpers.log('Installing {}.'.format(alias), 'info')
-        #            # helpers.execute('sudo apt-get install {}'.format(installer))
+        #            helpers.log("Installing {}.".format(alias), "info")
+        #            # helpers.execute("sudo apt-get install {}".format(installer))
         #    else:
-        #        helpers.log('{} already installed.'.format(alias), 'info')
+        #        helpers.log("{} already installed.".format(alias), "info")
 
     def __update(self):
-        # helpers.execute('echo "nameserver 8.8.8.8" | sudo tee /etc/resolv.conf > /dev/null', 'pipe') # hack for update
-        helpers.execute('sudo apt-get update --quiet')
+        # helpers.execute("echo "nameserver 8.8.8.8" | sudo tee /etc/resolv.conf > /dev/null", "pipe") # hack for update
+        helpers.execute("sudo apt-get update --quiet")
 
     def __upgrade(self):
-        helpers.execute('sudo apt-get upgrade')
+        helpers.execute("sudo apt-get upgrade")
 
     def __clean(self):
-        helpers.execute('sudo apt-get autoremove')
-        helpers.execute('sudo apt-get autoclean')
+        helpers.execute("sudo apt-get autoremove")
+        helpers.execute("sudo apt-get autoclean")
 
     def __remove(self, command):
-        helpers.execute('sudo apt-get purge {}'.format(command))
+        helpers.execute("sudo apt-get purge {}".format(command))
         self.__clean()
 
     def __get_installer_name(self, alias):
         installer_name = {
-            'vim': 'vim',
-            'curl': 'curl',
-            'git': 'git',
-            'google chrome': 'google-chrome-stable',
-            'vlc': 'vlc browser-plugin-vlc',
-            'sublime': 'sublime-text-installer',
-            'skype': 'skype',
-            'spotify': 'spotify-client'
+            "vim": "vim",
+            "curl": "curl",
+            "gdebi": "gdebi",
+            "dtrx": "dtrx",
+            "xclip": "xclip",
+            "git": "git",
+            "texlive": "texlive-latex-base texlive-fonts-recommended texlive-latex-extra",
+            "google chrome": "google-chrome-stable",
+            "vlc": "vlc browser-plugin-vlc",
+            "sublime": "sublime-text-installer",
+            "skype": "skype",
+            "spotify": "spotify-client",
+            "pandoc": "pandoc",
+            "meld": "meld",
+            "gitk": "gitk",
+            "nodejs": "nodejs",
+            "npm": "npm",
+            "nyancat": "nyancat",
+            "wine": "wine",
+            "viber": "viber.deb",
+            "dropbox": "dropbox.deb"
         }.get(alias, None)
 
         if installer_name is None:
-            raise RuntimeError("{} isn't an alias of a maintenance branch.".format(alias))
+            raise RuntimeError("{} isn't a supported installer name.".format(alias))
 
         return installer_name
 
     def __get_command_name(self, alias):
         command_name = {
-            'vim': 'vim',
-            'curl': 'curl',
-            'gdebi': 'gdebi',
-            'dtrx': 'dtrx',
-            'xclip': 'xclip',
-            'git': 'git',
-            'google chrome': 'google-chrome',
-            'vlc': 'vlc',
-            'sublime': 'subl',
-            'skype': 'skype',
-            'spotify': 'spotify'
+            "vim": "vim",
+            "curl": "curl",
+            "gdebi": "gdebi",
+            "dtrx": "dtrx",
+            "xclip": "xclip",
+            "git": "git",
+            "google chrome": "google-chrome",
+            "vlc": "vlc",
+            "sublime": "subl",
+            "skype": "skype",
+            "spotify": "spotify",
+            "texlive": "latex",
+            "pandoc": "pandoc",
+            "meld": "meld",
+            "gitk": "gitk",
+            "nodejs": "nodejs",
+            "npm": "npm",
+            "nyancat": "nyancat",
+            "wine": "wine",
+            "viber": "/opt/viber/Viber",
+            "dropbox": "dropbox"
         }.get(alias, None)
+
+        if command_name is None:
+            raise RuntimeError("{} isn't a supported command name.".format(alias))
 
         return command_name
 
     def __get_list_to_be_install(self):
         list = []
-        with open('list') as file:
+        with open("list") as file:
             for line in file:
-                if not line.lstrip().startswith('#'):
+                if not line.lstrip().startswith("#"):
                     list.append(line.rstrip());
 
         return filter(None, list)
